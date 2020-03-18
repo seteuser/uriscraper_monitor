e9103bb1260885eef483ac7606f6d1d9dedcd846
Andre <amsilva@gmail.com>
2020-03-18 05:38:51 -0300

    subindo projeto Matheus Victor

---
+++ wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿var uri = {
+
+    buscarRanking: function (parametro) {
+        $('#tableRanking').DataTable({
+            "destroy": true,
+            "bPaginate": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "autoWidth": false,
+            ajax: {
+                cache: false,
+                type: "GET",
+                url: "https://uri-webapi.herokuapp.com/api/uri/get",
+                data: { nome: parametro },
+                dataType: "json"
+            },
+            columns: [
+                { data: 'posicao' },
+                { data: 'rankingUri' },
+                { data: 'nome' },
+                { data: 'resolvidos' },
+                { data: 'tentativas' },
+                { data: 'submissoes' },
+                { data: 'pontos' }
+            ],
+            language: {
+                "sEmptyTable": "Nenhum registro encontrado",
+                "sLoadingRecords": "Carregando...",
+                "sProcessing": "Processando...",
+                "sZeroRecords": "Nenhum registro encontrado",
+                "sSearch": "Pesquisar"
+            },
+            columnDefs: [
+                {
+                    targets: "_all",
+                    className: 'dt-center'
+                }
+            ]
+        });
+    }
+};
+
+$(document).ready(function () {
+
+    $("#txtPesquisa").on('keyup', function (e) {
+        if (e.keyCode === 13 && $("#txtPesquisa").val().trim() != '') {
+            uri.buscarRanking($("#txtPesquisa").val().trim());
+        }
+    });
+
+});
